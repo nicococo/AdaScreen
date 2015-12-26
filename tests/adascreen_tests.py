8e4d20e2fdbd236f3c675a91f68cc2978d0f6738
nico <nicococo23@gmail.com>
2015-12-26 02:50:53 +0100

    @feat website image

--- tests/adascreen_tests.py
+++ tests/adascreen_tests.py
@@ -1,11 +1,9 @@
 import numpy as np
-import cvxopt as co
-
 from nose.tools import *
 
 
 def setup(exms=10, train=5, deps=1, add_intercept=True):
-
+    pass
 
 @with_setup(setup(exms=10, train=0, deps=1, add_intercept=True))
 def test_toy_data_setting():
