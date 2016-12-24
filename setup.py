89138293a92207a88edcd029dcd809390509fcbd
nicococo <sinnl0s>
2016-12-24 22:17:59 +0100

    setup builds cython code

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ try:
 except ImportError:
     from distutils.core import setup
 
-from distutils.extension import Extension
 from Cython.Build import cythonize
 
-import numpy as np
 
 config = {
     'name': 'adascreen',
